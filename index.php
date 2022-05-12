097955a2e22bfabfc6bd1c3513e83aa67ae14ac4
Abdelilah-Riouj <rioujabdelilah50@gmail.com>
2022-05-12 15:37:42 +0100

    ADD latest works in home page

--- index.php
+++ index.php
@@ -1,5 +1,40 @@
 <?php 
-    include "init.php";
-    include $temp . "header.php";
-    include $temp . "footer.php";
+  include "init.php";
+  include $temp . "header.php";
+?>
+<div class="container">
+  <div class="latest-works">
+    <h2>أخـر أعمالنـا</h2>
+    <div class="cards">
+      <div class="card" style="width: 18rem;">
+        <img src="https://your-comics.com/media/image/img_news_10_3_lang62.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">عنـوان التصميـم</h5>
+          <p class="card-text">بعض الأمثلة السريعة للنصوص للبناء على عنوان البطاقة وتشكيل الجزء الأكبر من محتوى البطاقة.</p>
+          <a href="#" class="btn btn-primary">مشاهـدة المزيـد</a>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem;">
+        <img src="https://your-comics.com/media/image/img_news_10_3_lang62.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">عنـوان التصميـم</h5>
+          <p class="card-text">بعض الأمثلة السريعة للنصوص للبناء على عنوان البطاقة وتشكيل الجزء الأكبر من محتوى البطاقة.</p>
+          <a href="#" class="btn btn-primary">مشاهـدة المزيـد</a>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem;">
+        <img src="https://your-comics.com/media/image/img_news_10_3_lang62.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">عنـوان التصميـم</h5>
+          <p class="card-text">بعض الأمثلة السريعة للنصوص للبناء على عنوان البطاقة وتشكيل الجزء الأكبر من محتوى البطاقة.</p>
+          <a href="#" class="btn btn-primary">مشاهـدة المزيـد</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="join-us">
+  </div>
+</div>
+<?php
+  include $temp . "footer.php";
 ?>
