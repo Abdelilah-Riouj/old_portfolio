dcd8b8218c28260e8552e0e6536068e1e839a2a5
Abdelilah-Riouj <rioujabdelilah50@gmail.com>
2022-05-12 23:57:33 +0100

    ADD footer & copyright

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,5 +1,88 @@
 
-    
+<footer>
+    <div class="cate">
+        <div class="aboutOur-team">
+            <h3>حـول <span> الفريـق</span></h3>
+            <div class="list">
+               <ul>
+                    <li>فريـق العمـل</li>
+                    <li>أهدافنـا مستقبـلا</li>
+                    <li>أوقـات العمـل</li>
+                    <li>إدعـم الموقـع</li>
+                </ul> 
+            </div>
+            
+        </div>
+        <div class="help">
+            <div class="list">
+                <h3>البحـث عـن <span> المساعـدة</span></h3>
+                <ul>
+                    <li>مشكلـة فـي الحسـاب</li>
+                    <li>رمـز التحقـق لا يصـل</li>
+                    <li>تـاكيـد الحسـاب</li>
+                    <li>توثيـق الحسـاب</li>
+                </ul>
+            </div>
+        </div>
+        <div class="help">
+            <div class="list">
+                <h3>فـروع <span> الموقـع</span></h3>
+                <ul>
+                    <li>الملـف الشخصـي</li>
+                    <li>الاعمـال</li>
+                    <li>الرائـج حاليـا</li>
+                    <li>إحجـز تصميمـك</li>
+                </ul>
+            </div>
+        </div>
+        <div class="contactUs">
+            <div class="contact">
+                <h3>تواصـل <span> معنـا</span></h3>
+                <div class="row mb-2">
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="إسـم الشخصـي">
+                    </div>
+                    <div class="col">
+                        <input type="email" class="form-control" placeholder="بريـدك اﻻلكترونـي">
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <div class="col">
+                        <input type="number" class="form-control" placeholder="رقـم هاتفـك">
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="عنـوانـك">
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <div class="col">
+                        <textarea class="form-control" name="" id="" cols="10" rows="5" placeholder="رسالتـك"></textarea>
+                    </div>
+                </div>
+                <button type="submit" class="btn">
+                    <span>
+                        <i class="fa fa-phone"></i>
+                    </span>
+                    <label for="">إرسال</label>
+                </button>
+            </div>
+        </div>
+    </div>
+    <div class="copyright">
+        <h4>العنـوان</h4>
+        <ul>
+            <li>
+                سياسـة المحتوى |                 
+            </li>
+            <li>
+                       شـروط الخدمـة |                 
+            </li>
+            <li>
+                        الخصوصيـة                   
+            </li>
+        </ul>
+    </div>
+</footer>
     <script src="<?php echo $jsLib;?>jquery-3.6.0.min.js"></script>
     <script src="<?php echo $jsLib; ?>bootstrap.min.js"></script>
     <script src="<?php echo $jsLib;?>all.min.js"></script>
