d7a9f3f982a494c1793e7be33d90cae59e7e47e1
Abdelilah-Riouj <rioujabdelilah50@gmail.com>
2022-05-17 13:18:39 +0100

    Add Left Bar To Profile

--- profile.php
+++ profile.php
@@ -3,7 +3,28 @@
     $NoSlider = "";
     include $temp . "header.php";
 ?>
+<<<<<<< HEAD
 
+=======
+<div class="container-fluid">
+    <div class="row">
+        <div class="sliderProfile col-2">
+            <ul>
+                <li class="active"><i class="fa fa-user-gear"></i> المعلومـات الشخصيـة</li>
+                <li><i class="fa fa-link"></i> روابـط الشخصيـة</li>
+                <li><i class="fa fa-share"></i> مشاركـة مشـروع</li>
+                <li><i class="fa fa-eye"></i> الخصوصيـة</li>
+                <li><i class="fa fa-ban"></i> المحظـورون</li>
+                <li><i class="fa fa-box-archive"></i> الارشيـف</li>
+                <li><i class="fa fa-trash"></i> حـدف الحســاب</li>
+                <li><i class="fa fa-triangle-exclamation"></i> البريـد الـوارد</li>
+            </ul>
+        </div>
+        <div class="content col-10 row">
+        </div>
+    </div>
+</div>
+>>>>>>> 45c7983 (Add left bar to profile)
 <?php
     include $temp . "footer.php";
 ?>
